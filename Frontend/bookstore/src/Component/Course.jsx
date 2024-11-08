05eefbc240159a092650a357a162fd8f1ab1da5f
srijankishu <140534769+srijankishu@users.noreply.github.com>
2024-11-08 20:57:04 +0530

    Update Course.jsx

--- Frontend/bookstore/src/Component/Course.jsx
+++ Frontend/bookstore/src/Component/Course.jsx
@@ -9,7 +9,7 @@ const Course = () => {
   useEffect(() =>{
     const getBook = async () => {
     try{
-      const res = await axios.get(https://book-website-gamma.vercel.app/book');
+      const res = await axios.get('http://localhost:4001/book');
     
       setbook(res.data);
 
